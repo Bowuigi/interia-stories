1bcab1101f8d7c856f1cc5949c741e33295658a1
Bowuigi <matucrimella@gmail.com>
2021-11-08 14:28:49 -0300

    Added a bunch of stories

--- stories/genindex.sh
+++ stories/genindex.sh
@@ -1,10 +1,12 @@
-
 sed -n '1,/STORIES_START/p' ../index.html > tmp.html
 
+awk 'NR>2{gsub("\t+","\t");print}' info.tsv > info-tmp.tsv
+
 awk -F '\t' \
-	'{print "<a href=\"/interia-stories/stories/" $1 "\" download=\"" $1 "\">\n" "\t<p class=\"historia\">" $2 "</p>\n\t<p class=\"autor\">Por " $3 "</p>\n\t<p class=\"descripcion\">" $4 "</p>\n</a>"}' info.tsv \
+	'{print "\t\t\t\t<a href=\"/interia-stories/stories/" $1 "\" download=\"" $1 "\">\n\t\t\t\t\t<p class=\"historia\">" $2 "</p>\n\t\t\t\t\t<p class=\"autor\">Por " $3 "</p>\n\t\t\t\t\t<p class=\"descripcion\">" $4 "</p>\n\t\t\t\t</a>"}' info-tmp.tsv \
 	>> tmp.html
 
 sed -n '/STORIES_END/,$p' ../index.html >> tmp.html
 
+rm info-tmp.tsv
 mv tmp.html ../index.html
